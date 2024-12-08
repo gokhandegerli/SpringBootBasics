f2064b456c7cf4d1cac3ef459ab77d59d0c57e22
gokhandegerli <gokhan.degerli.sd@gmail.com>
2024-12-08 23:08:26 +0300

    actuator health and metrics enabled

--- src/main/java/com/degerli/SpringBootBasics/domain/hashcodequals/User.java
+++ src/main/java/com/degerli/SpringBootBasics/domain/hashcodequals/User.java
@@ -5,19 +5,22 @@ import lombok.Getter;
 
 @Getter
 public class User {
-  // Getters and setters (if needed)
   private int id;
   private String name;
   private int age;
 
-  // Constructor
   public User(int id, String name, int age) {
     this.id = id;
     this.name = name;
     this.age = age;
   }
 
-  // Override equals() method
+  /*  We override both of equals and hashcode methods since If two objects are equal
+   according to the equals(Object) method, they must have the same hash code. This is crucial
+   for the proper functioning of hash-based collections. If two objects are not equal, they
+   may have the same or different hash codes. However, it is generally good practice to try
+   to minimize collisions (different objects having the same hash code) for performance
+   reasons. Below, that is why we use same fields for both equals and hashing logics */
   @Override
   public boolean equals(Object o) {
     if (this == o) {
@@ -30,7 +33,11 @@ public class User {
     return id == user.id && age == user.age && Objects.equals(name, user.name);
   }
 
-  // Override hashCode() method
+  /*Override hashCode() method
+  Objects.hash(...): This is a static utility method that takes one or more arguments (in
+  this case, id, name, and age) and computes a hash code for them.
+  */
+
   @Override
   public int hashCode() {
     return Objects.hash(id, name, age);
